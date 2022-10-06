812f98d249f085239027b90569da5d557f02663e
Astargisco <edubini@studenti.uninsubria.it>
2022-10-06 09:26:47 +0200

    verify compatibility between github & gitlab

--- src/main/java/com/example/springbootfirstproject/SPringBootFirstProjectApplication.java
+++ src/main/java/com/example/springbootfirstproject/SPringBootFirstProjectApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SPringBootFirstProjectApplication {
-//commento aggiunto per verificare la compatibilità e aggiornamento delle modifiche al progetto tra github e gitlab 1
     public static void main(String[] args) {
         SpringApplication.run(SPringBootFirstProjectApplication.class, args);
     }
